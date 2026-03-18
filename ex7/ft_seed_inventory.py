f14f4862864b0288ecc558b5e0b320dc5844cffb
David Garcia Solis <davgarc4@c2r12s6.42madrid.com>
2026-03-18 19:37:56 +0100

    terminado

--- ex7/ft_seed_inventory.py
+++ ex7/ft_seed_inventory.py
@@ -1,17 +1,11 @@
-# **************************************************************************** #
-#                                                                              #
-#                                                         :::      ::::::::    #
-#    ft_seed_inventory.py                               :+:      :+:    :+:    #
-#                                                     +:+ +:+         +:+      #
-#    By: davgarc4 <davgarc4@student.42madrid.com    +#+  +:+       +#+         #
-#                                                 +#+#+#+#+#+   +#+            #
-#    Created: 2026/03/16 20:31:15 by davgarc4          #+#    #+#              #
-#    Updated: 2026/03/16 20:38:39 by davgarc4         ###   ########.fr        #
-#                                                                              #
-# **************************************************************************** #
 
-def	ft_seed_inventory(seed_type: str, quantity: int, unit: str) -> None:
+
+def ft_seed_inventory(seed_type: str, quantity: int, unit: str) -> None:
 	if (unit == "packets"):
 		print(seed_type.capitalize(), "seeds:", quantity, unit, "packets")
-if __name__ == "__main__":
-	ft_seed_inventory()
+	elif (unit == "grams"):
+		print(seed_type.capitalize(), "seeds:", quantity, unit, "grams")
+	elif (unit == "area"):
+		print(seed_type.capitalize(), "seeds:", quantity, unit, "area")
+
+ft_seed_inventory("tomato", 15, "packets")
